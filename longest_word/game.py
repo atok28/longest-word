d640e9d97c39b155e1f7547796b0c4f6057b5b51
Artur Tokarski <artur.tokarski8@gmail.com>
2025-02-23 19:23:48 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,7 @@
 import string
 import random
+import requests
+
 
 
 class Game:
@@ -18,4 +20,10 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
